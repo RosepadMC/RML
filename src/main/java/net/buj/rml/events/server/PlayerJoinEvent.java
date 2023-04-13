5d0d417c50caac809b5c6f608b9994538183d4db
Buj <buj@buj-dev.ru>
2023-04-13 19:11:10 +0700

    Made PlayerJoinEvent/PlayerLeaveEvent cancelable

--- src/main/java/net/buj/rml/events/server/PlayerJoinEvent.java
+++ src/main/java/net/buj/rml/events/server/PlayerJoinEvent.java
@@ -1,16 +1,27 @@
 package net.buj.rml.events.server;
 
-import net.buj.rml.events.Event;
+import net.buj.rml.annotations.NotNull;
+import net.buj.rml.events.CancellableEvent;
 
 
-public final class PlayerJoinEvent extends Event {
+public final class PlayerJoinEvent extends CancellableEvent {
     private final String username;
+    private String message;
 
-    public PlayerJoinEvent(String username) {
+    public PlayerJoinEvent(String username, String joinMessage) {
         this.username = username;
+        this.message = joinMessage;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getJoinMessage() {
+        return message;
+    }
+
+    public void setJoinMessage(@NotNull String message) {
+        this.message = message;
     }
 }
--- src/main/java/net/buj/rml/events/server/PlayerLeaveEvent.java
+++ src/main/java/net/buj/rml/events/server/PlayerLeaveEvent.java
@@ -1,16 +1,27 @@
 package net.buj.rml.events.server;
 
-import net.buj.rml.events.Event;
+import net.buj.rml.annotations.NotNull;
+import net.buj.rml.events.CancellableEvent;
 
 
-public final class PlayerLeaveEvent extends Event {
+public final class PlayerLeaveEvent extends CancellableEvent {
     private final String username;
+    private String message;
 
-    public PlayerLeaveEvent(String username) {
+    public PlayerLeaveEvent(String username, String leaveMessage) {
         this.username = username;
+        this.message = leaveMessage;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getLeaveMessage() {
+        return message;
+    }
+
+    public void setLeaveMessage(@NotNull String message) {
+        this.message = message;
     }
 }
