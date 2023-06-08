b6363e25890762757539818ff4fee8d2f11f113a
Buj <buj@buj-dev.ru>
2023-06-08 11:28:45 +0700

    Replace `String` with `Path`

--- src/main/java/net/buj/rml/MinecraftImpl.java
+++ src/main/java/net/buj/rml/MinecraftImpl.java
@@ -1,14 +1,16 @@
 package net.buj.rml;
 
+import java.nio.file.Path;
+
 public interface MinecraftImpl {
     int[] getVersion();
     String getVersionTag();
     String getVersionString();
-    
+
     //TODO: Probably should be moved to the Environment enum
     /**
      * Get current environment's working directory.
      * @return A string depicting an absolute file path to the game's working directory. If current{@link Environment} is <code>CLIENT</code>, returns the path to <code>.minecraft</code>. Otherwise returns the path to the directory that contains <code>server.properties</code>
      */
-    String getGameDirectory();
+    Path getGameDirectory();
 }
